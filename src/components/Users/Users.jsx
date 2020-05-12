167342634fab14efc647b207a458a9a8ec1983b7
Roman P <rpidlis@gmail.com>
2020-05-12 22:03:11 +0300

    add classes component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,35 +2,34 @@ import React from "react";
 import s from './Users.module.css'
 import * as axios from 'axios';
 
-let Users = (props) => {
-
-    if (props.users.length === 0) {
+export default class Users extends React.Component {
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items);
-            });
+            this.props.setUsers(response.data.items);
+        });
     }
-    ;
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt='' className={s.usersPhoto}/>
+                            <img src='../../assets/images/ava.jpg' alt='' className={s.usersPhoto}/>
                         </div>
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(u.id)
+                                    this.props.unfollow(u.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    props.follow(u.id)
+                                    this.props.follow(u.id)
                                 }}>Follow</button>
                             }
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -40,10 +39,58 @@ let Users = (props) => {
                             {/*<div>{u.location.city}</div>*/}
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
-};
+                    </div>)
+                }
+            </div>
+        );
+        ;
+    }
+}
 
-export default Users;
+
+// let Users = (props) => {
+//
+//     if (props.users.length === 0) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//             props.setUsers(response.data.items);
+//         });
+//     }
+//     ;
+//
+//     return (
+//         <div>
+//             {
+//                 props.users.map(u => <div key={u.id}>
+//                     <span>
+//                         <div>
+//                             <img src={u.photoUrl} alt='' className={s.usersPhoto}/>
+//                         </div>
+//                         <div>
+//                             {u.followed
+//                                 ? <button onClick={() => {
+//                                     props.unfollow(u.id)
+//                                 }}>Unfollow</button>
+//                                 : <button onClick={() => {
+//                                     props.follow(u.id)
+//                                 }}>Follow</button>
+//                             }
+//                         </div>
+//                     </span>
+//                     <span>
+//                         <span>
+//                             <div>{u.name}</div>
+//                             <div>{u.name}</div>
+//                             <div>{u.status}</div>
+//                         </span>
+//                         <span>
+//                             {/*<div>{u.location.county}</div>*/}
+//                             {/*<div>{u.location.city}</div>*/}
+//                         </span>
+//                     </span>
+//                 </div>)
+//             }
+//         </div>
+//     );
+// };
+//
+// export default Users;
